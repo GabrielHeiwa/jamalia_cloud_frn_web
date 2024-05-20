e64153408bf6e4172cbdfe36a722d2a057d9dc04
gabrielheiwa <gabriel.paz@beansoftwares.com.br>
2024-05-20 18:11:21 -0300

    change: recaptcha secret keys

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -14,14 +14,14 @@ return [
     /*
      * Use a custom secret key, we use our public one by default
      */
-    'secret_key' => env('RECAPTCHA_SECRET_KEY', '6LcJcjwUAAAAALOcDJqAEYKTDhwELCkzUkNDQ0J5'),
-    '_shipped_secret_key' => '6LcJcjwUAAAAALOcDJqAEYKTDhwELCkzUkNDQ0J5',
+    'secret_key' => env('RECAPTCHA_SECRET_KEY', '6LfMWN8pAAAAAIBbn4LE_Xg3OQ5I0-le_hd9uipx'),
+    '_shipped_secret_key' => '6LfMWN8pAAAAAIBbn4LE_Xg3OQ5I0-le_hd9uipx',
 
     /*
      * Use a custom website key, we use our public one by default
      */
-    'website_key' => env('RECAPTCHA_WEBSITE_KEY', '6LcJcjwUAAAAAO_Xqjrtj9wWufUpYRnK6BW8lnfn'),
-    '_shipped_website_key' => '6LcJcjwUAAAAAO_Xqjrtj9wWufUpYRnK6BW8lnfn',
+    'website_key' => env('RECAPTCHA_WEBSITE_KEY', '6LfMWN8pAAAAALCcJGS2jZxQJz8mza_H7IIarUhS'),
+    '_shipped_website_key' => '6LfMWN8pAAAAALCcJGS2jZxQJz8mza_H7IIarUhS',
 
     /*
      * Domain verification is enabled by default and compares the domain used when solving the captcha
